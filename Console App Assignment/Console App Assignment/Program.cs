21ca18547759bd9cbaab04a158ee4061a43cb454
dalton704 <lokuma96@gmail.com>
2022-03-22 14:40:24 -0700

    Made new assignments

---
+++ Console App Assignment/Console App Assignment/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Console_App_Assignment
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            string[] favFoods = new string[] { "Spaghetti", "Tacos", "Chicken Alfredo", "Pizza" };
+            Console.WriteLine("What are your four favorite food?");
+            for (int i = 0; i < favFoods.Length; i++)
+            {
+                favFoods[i] = favFoods[i] + " " + Console.ReadLine();
+            }
+            
+            foreach (string food in favFoods)
+            {
+                Console.WriteLine(food);
+       
+            }
+            Console.ReadLine();
+        }
+    }
+}
---
+++ Console App Part 4 Assignment/Console App Part 4 Assignment/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Console_App_Part_4_Assignment
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //List<string> Greeting = new List<string>()
+            //{
+            //    "Hello", "Hi", "Hello", "Sup", "Yo", "Hola"
+            //};
+
+            //Console.WriteLine("Please input a greeting to search for:");
+            //string search = Convert.ToString(Console.ReadLine());
+            //for (int i = 0; i < Greeting.Count; i++)
+            //{
+            //    if (search == Greeting[i])
+            //    {
+            //        Console.WriteLine("That greeting is located at index " + i);
+            //    }
+            //    else
+            //    {
+            //        Console.WriteLine("Sorry, your input is not on the list.");
+            //    }
+            //}
+            //Console.ReadLine();
+
+            List<string> similar = new List<string>()
+            {
+                "Hello", "Hi", "Hey", "Hello", "Hey", "Wassup"
+            };
+            List<string> repeat = new List<string>();
+            foreach (string same in similar)
+            {
+                Console.WriteLine(same);
+                if (repeat.Contains(same))
+                {
+                    Console.WriteLine("This greeting was stated before.");
+                }
+                else
+                {
+                    Console.WriteLine("This greeting was not stated before");
+                }
+                repeat.Add(same);
+            }
+            Console.ReadLine();
+        }
+    }
+}
