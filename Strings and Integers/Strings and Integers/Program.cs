af6860e7c408bec8f60cbf1070d6481a44632314
dalton704 <lokuma96@gmail.com>
2022-03-22 15:36:34 -0700

    Added Stings and Integers Assignment

---
+++ Strings and Integers/Strings and Integers/Program.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Strings_and_Integers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                List<int> numberList = new List<int>
+            {
+                12, 4, 6, 9, 32, 7, 11, 2
+            };
+                Console.WriteLine("Please input a number to be divided by.");
+                int divide = Convert.ToInt32(Console.ReadLine());
+                for (int i = 0; i < numberList.Count; i++)
+                {
+                    int result = numberList[i] / divide;
+                    string j = Convert.ToString(i);
+                    Console.WriteLine(numberList[i] + " divided by " + divide + " equals " + result);
+                }
+                
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+            finally
+            {
+                Console.ReadLine();
+            }
+        }
+    }
+}
+
+
+        //try
+        //{
+        //    Console.WriteLine("Pick a number.");
+        //    int numberOne = Convert.ToInt32(Console.ReadLine());
+        //    Console.WriteLine("Pick a second number.");
+        //    int numberTwo = Convert.ToInt32(Console.ReadLine());
+        //    Console.WriteLine("Dividing the two...");
+        //    int numberThree = numberOne / numberTwo;
+        //    Console.WriteLine(numberOne + " divided by " + numberTwo + " equals " + numberThree);
+        //    Console.ReadLine();
+        //}
+        //catch (FormatException ex)
+        //{
+        //    Console.WriteLine(ex.Message);
+        //}
+        //catch (DivideByZeroException ex)
+        //{
+        //    Console.WriteLine("Please don't divide by zero.");
+        //}
+        //catch (Exception ex)
+        //{
+        //    Console.WriteLine(ex.Message);
+        //}
+        //finally
+        //{
+        //    Console.ReadLine();
+        //}
+
