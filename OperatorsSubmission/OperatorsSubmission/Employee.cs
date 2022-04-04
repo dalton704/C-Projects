183c611bd922ea73cd335dfdc5073d26929cdb55
dalton704 <lokuma96@gmail.com>
2022-04-04 15:26:26 -0700

    Overloaded the "==" operator

---
+++ OperatorsSubmission/OperatorsSubmission/Employee.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OperatorsSubmission
+{
+    public class Employee
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public int Id { get; set; }
+
+        // If emp1 and emp2 id's are equal will return true.
+        public static bool operator == (Employee emp1, Employee emp2)
+        {
+            return emp1.Id == emp2.Id;
+        }
+
+        // If emp1 and emp2 id's are not equal will return true
+        public static bool operator != (Employee emp1, Employee emp2)
+        {
+            return emp1.Id != emp2.Id;
+        }
+    }
+}
---
+++ OperatorsSubmission/OperatorsSubmission/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OperatorsSubmission
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Overloading the "==" operator to check if the employee ID's are equal.
+
+            Employee emp1 = new Employee() { Id = 1 };
+            Employee emp2 = new Employee() { Id = 2 };
+            Console.WriteLine(emp1 == emp2);
+            Console.ReadLine();
+        }
+    }
+}
