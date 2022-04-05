5610571cc03607a07a5438e27bd772d8c441cd48
dalton704 <lokuma96@gmail.com>
2022-04-05 13:26:12 -0700

    Created a struct value type

---
+++ StructAssignment/StructAssignment/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StructAssignment
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Instantiating the Number Struct
+            Number number = new Number();
+
+            // Assigning a decimal to the Amount value
+            number.Amount = 25.1m;
+            
+            // Writing the amount to the console
+            Console.WriteLine(number.Amount);
+            Console.ReadLine();
+
+        }
+    }
+}
