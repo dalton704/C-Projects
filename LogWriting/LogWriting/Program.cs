268e6db0a86bdecdd7e845554573a03527057375
dalton704 <lokuma96@gmail.com>
2022-04-06 15:31:36 -0700

    Created a log recorder

---
+++ LogWriting/LogWriting/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace LogWriting
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Please type a number:");
+            int number = Convert.ToInt32(Console.ReadLine());
+            using (StreamWriter file = new StreamWriter(@"C:\Users\lokum\source\repos\log.txt", true))
+            {
+                file.WriteLine(number);
+            }
+            Console.WriteLine("Your number has been recorded.");
+            Console.ReadLine();
+        }
+    }
+}
