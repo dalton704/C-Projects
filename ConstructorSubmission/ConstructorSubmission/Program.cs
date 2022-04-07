ac718547aa7a062334cf2f246e2734a9c5fb54a3
dalton704 <lokuma96@gmail.com>
2022-04-06 17:18:07 -0700

    Created constructors

---
+++ ConstructorSubmission/ConstructorSubmission/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConstructorSubmission
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            const string cons1 = "Your name is: ";
+            const string cons2 = "Your favorite number is: ";
+            Console.WriteLine("Enter your name or favorite number: ");
+            var answer = Console.ReadLine();
+            if (float.TryParse(answer, out float x))
+            {
+                Console.WriteLine(cons2);
+                Chain newChain = new Chain(x);
+            }
+            else
+            {
+                Console.WriteLine(cons1);
+                Chain newChain = new Chain(answer);
+            }
+            Console.ReadLine();
+        }
+    }
+}
